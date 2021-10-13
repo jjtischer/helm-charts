03b6e667695be3faef6c1be93f649f162ad9ebc3
Bart Van Bos <bartvanbos@gmail.com>
2021-10-13 22:02:43 +0200

    added helper makefile

---
+++ scripts/istio.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=$(pwd)
+source ${ROOT_DIR}/scripts/environment.sh
+
+KUBECONFIG=${ROOT_DIR}/${EKS_CLUSTER_NAME}-kubeconfig.yaml
+
+if [[ $1 = "install" ]]; then
+  istioctl --kubeconfig ${KUBECONFIG} operator init
+  exit 0
+fi
+
+if [[ $1 = "istioctl" ]]; then
+  curl -sL https://istio.io/downloadIstioctl | ISTIO_VERSION=${ISTIO_VERSION} sh - && \
+  sudo cp ~/.istioctl/bin/istioctl /usr/local/bin
+  exit 0
+fi
+
+echo "please specify action ./istio.sh install/istioctl"
+exit 1
