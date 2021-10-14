13fde4b08f841c016ef0ae91bc7f0821fe576461
Bart Van Bos <bartvanbos@gmail.com>
2021-10-14 04:25:15 +0200

    expose kiali on port 80 iso 20001

--- scripts/bookinfo/bookinfo.sh
+++ scripts/bookinfo/bookinfo.sh
@@ -3,32 +3,32 @@
 source ../../scripts/environment.sh
 set -o xtrace
 
-KUBECONFIG=../../${EKS_CLUSTER_NAME}-kubeconfig.yaml
+export KUBECONFIG=../../${EKS_CLUSTER_NAME}-kubeconfig.yaml
 BOOKINFO_NAMESPACE=bookinfo
 AM_NAMESPACE=istio-system
 
 if [[ $1 = "install" ]]; then
-	kubectl --kubeconfig ${KUBECONFIG} apply -f ./namespace.yaml || true
-	kubectl --kubeconfig ${KUBECONFIG} -n ${BOOKINFO_NAMESPACE} apply -f ./deploy
+	kubectl apply -f ./namespace.yaml || true
+	kubectl -n ${BOOKINFO_NAMESPACE} apply -f ./deploy
 	sleep 2
-  kubectl --kubeconfig ${KUBECONFIG} -n ${BOOKINFO_NAMESPACE} wait --timeout=2m --for=condition=Ready pods --all
+  kubectl -n ${BOOKINFO_NAMESPACE} wait --timeout=2m --for=condition=Ready pods --all
   exit 0
 fi
 
 if [[ $1 = "uninstall" ]]; then
-	kubectl --kubeconfig ${KUBECONFIG} -n ${BOOKINFO_NAMESPACE} delete -f ./deploy
-	kubectl --kubeconfig ${KUBECONFIG} delete namespace ${BOOKINFO_NAMESPACE}
+	kubectl -n ${BOOKINFO_NAMESPACE} delete -f ./deploy
+	kubectl delete namespace ${BOOKINFO_NAMESPACE}
   exit 0
 fi
 
 if [[ $1 = "info" ]]; then
-	kubectl --kubeconfig ${KUBECONFIG} -n ${BOOKINFO_NAMESPACE} get all -o wide
+	kubectl -n ${BOOKINFO_NAMESPACE} get all -o wide
   exit 0
 fi
 
 if [[ $1 = "restart" ]]; then
-	kubectl --kubeconfig ${KUBECONFIG} -n ${BOOKINFO_NAMESPACE} rollout restart deployments
-	kubectl --kubeconfig ${KUBECONFIG} -n ${BOOKINFO_NAMESPACE} wait --timeout=2m --for=condition=Ready pods --all
+	kubectl -n ${BOOKINFO_NAMESPACE} rollout restart deployments
+	kubectl -n ${BOOKINFO_NAMESPACE} wait --timeout=2m --for=condition=Ready pods --all
   exit 0
 fi
 
