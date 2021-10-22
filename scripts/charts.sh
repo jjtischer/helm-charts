dfc26cafb5690b04c7674fa0bf24dfb857dd60b6
Bart Van Bos <bartvanbos@gmail.com>
2021-10-22 15:56:02 +0200

    added chart for istio+kic

--- scripts/charts.sh
+++ scripts/charts.sh
@@ -5,9 +5,10 @@ source ${ROOT_DIR}/scripts/environment.sh
 set -o xtrace
 
 if [[ $1 = "package" ]]; then
-  cr package charts/elastic-stack
-  cr package charts/grafana-stack
-  cr package charts/kiali-stack
+  # cr package charts/elastic-stack
+  # cr package charts/grafana-stack
+  cr package charts/istio-nginx-kic
+  # cr package charts/kiali-stack
   exit 0
 fi
 
